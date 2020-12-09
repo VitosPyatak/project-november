f9e5364457840a5065525994605f2607719550cf
Victor <pyatakvictor2@gmail.com>
2020-12-09 20:33:26 +0200

    Adding register view

---
+++ project-november/Views/RegisterView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import Foundation
+
+struct RegisterView: View {
+    @ObservedObject var viewModel = RegisterFormViewModel()
+    
+    var body: some View {
+        Form {
+            ForEach(viewModel.fields.indices) { fieldIndex in
+                FormTextField(getTextFieldBinding(by: fieldIndex))
+            }
+            
+            Button(action: onCreateButtonPressed) {
+                Text(ButtonPlacehoder.createUser)
+            }
+        }
+        .navigationTitle(NavigationTitles.createUser)
+    }
+    
+    private func onCreateButtonPressed() {
+        viewModel.validateFields()
+        if viewModel.areAllFieldsValid() {
+            UserService.save(from: viewModel.fields)
+        }
+    }
+    
+    
+    private func getTextFieldBinding(by index: Int) -> Binding<TextFieldInput> {
+        $viewModel.fields[index]
+    }
+}
