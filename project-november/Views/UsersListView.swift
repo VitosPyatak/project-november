4cad5b2ccb3185c6e2085c8456f70dfe8a0e0996
Victor <pyatakvictor2@gmail.com>
2020-12-18 18:46:34 +0200

    Removing unnecessary comments

--- project-november/Views/UsersListView.swift
+++ project-november/Views/UsersListView.swift
@@ -3,8 +3,6 @@ import SwiftUI
 struct UsersListView: View {
     @State private var users: [User]?
     
-    
-    
     var body: some View {
         HStack {
             if let allUsers = users {
@@ -25,29 +23,3 @@ struct UsersListView: View {
         users = UserService.getAll()
     }
 }
-
-
-//struct UserListPreview: PreviewProvider {
-//    
-//     private var users = [UserEntity]()
-//    
-//    
-//    init() {
-//        let user1 = UserEntity()
-//        user1.firstName = "test"
-//        user1.lastName = "test"
-//        user1.phoneNumber = "123123123"
-//        
-//        let user2 = UserEntity()
-//        user2.firstName = "test"
-//        user2.lastName = "test"
-//        user2.phoneNumber = "123123123"
-//        
-//        users = [user1, user2]
-//    }
-//    
-//    
-//    static var previews: some View {
-//        UsersListView()
-//    }
-//}
